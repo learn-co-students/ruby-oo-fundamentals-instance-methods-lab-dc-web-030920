bbfe70b71a5d5b4b52dbca00e5c36210f9506dc6
rasdinho <ras.dinho7@gmail.com>
2020-03-10 12:12:23 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person 
+    def talk
+    puts "Hello World!"
+    end
+end
+ada = Person.new
+ada.talk
+
+class Person
+    def walk
+        puts "The Person is walking"
+    end
+end
+ada = Person.new
+ada.walk
